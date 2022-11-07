4945724f49d75f9f22debc61db78c011af4036ec
Josue-Diogo-Pedro <pedrojosuediogo@gmail.com>
2022-11-07 08:21:05 +0100

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+function createGame(player1, hour, player2) {
+  return `
+    <li>
+      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+      <strong>${hour}</strong>
+      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+    </li>
+  `
+}
+
+let delay = -0.7;
+function createCard(date, day, games) {
+  delay += 0.7;
+  return `
+    <div class="card" style="animation-delay: ${delay}s">
+      <h2>${date} <span>${day}</span></h2>
+      <ul>
+        ${games}
+      </ul>
+    </div>
+  `
+}
+
+document.querySelector("#app").innerHTML = `
+  <header>
+    <img src="./assets/logo.svg" alt="Logo da NLW" />
+  </header>
+  <main id="cards">
+    ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
+    ${createCard(
+      "28/11",
+      "segunda",
+      createGame("switzerland", "13:00", "brazil") +
+      createGame("portugal", "16:00", "uruguai")
+    )}
+    ${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))}
+  </main>
+`
